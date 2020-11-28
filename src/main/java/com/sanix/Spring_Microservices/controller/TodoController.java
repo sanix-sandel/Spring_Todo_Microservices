d02226d8eae74482c32fa3fd7ec3ba2aa1ba3170
sanix <sanicksikani@gmail.com>
2020-11-29 02:42:33 +0300

    Hateos 2.0

--- src/main/java/com/sanix/Spring_Microservices/controller/TodoController.java
+++ src/main/java/com/sanix/Spring_Microservices/controller/TodoController.java
@@ -4,12 +4,19 @@ import com.sanix.Spring_Microservices.domain.ToDo;
 import com.sanix.Spring_Microservices.exceptions.TodoNotFoundException;
 import com.sanix.Spring_Microservices.service.ToDoService;
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.core.io.Resource;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 
 @RestController
 public class TodoController {
@@ -23,12 +30,15 @@ public class TodoController {
     }
 
     @GetMapping("/users/{name}/todos/{id}")
-    public ToDo retrieveTodos(@PathVariable String name, @PathVariable int id){
+    public EntityModel<ToDo> retrieveTodos(@PathVariable String name, @PathVariable int id){
         ToDo todo=toDoService.retrieveTodo(id);
         if(todo==null){
             throw new TodoNotFoundException("ToDo not found");
         }
-        return todo;
+        EntityModel<ToDo> toDoResource=EntityModel.of(todo);
+        WebMvcLinkBuilder linkTo=WebMvcLinkBuilder.linkTo(methodOn(this.getClass()).retrieveTodos(name));
+        toDoResource.add(linkTo.withRel("parent"));
+        return toDoResource;
     }
 
     @PostMapping("/users/{name}/todos")
